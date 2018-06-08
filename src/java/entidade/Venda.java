4b67544a6b4a9b0489a22eaac90fbccad8c645be
LeoLeiria <leoleiria@gmail.com>
2018-06-07 22:01:09 -0300

    Criação de Pre layout

--- src/java/entidade/Venda.java
+++ src/java/entidade/Venda.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -22,6 +23,7 @@ public class Venda implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @ManyToOne
     private Cliente Cliente;
     @OneToMany
     private List<Peixe> ListPeixe;
